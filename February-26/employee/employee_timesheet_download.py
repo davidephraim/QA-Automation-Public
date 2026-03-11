d0f56d4aa7d5a1a309d2aeef4e81900bcd407683
David E. <d.eph.me@gmail.com>
2026-03-11 13:59:08 +0700

    Automate timesheet (DS on): Select format, create, approve, reject

--- February-26/employee/employee_timesheet_download.py
+++ February-26/employee/employee_timesheet_download.py
@@ -27,7 +27,7 @@ def login_page(page, env):
 
 # ================= DOWNLOAD LATEST TIMESHEET =================
 
-def download_timesheet(playwright, env_config):
+def download_timesheet(playwright, env_config, company_format, timesheet_config):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
@@ -37,19 +37,20 @@ def download_timesheet(playwright, env_config):
 
     # DOWNLOAD TIMESHEET
     page.get_by_role("link", name="Timesheet").click()
-    page.locator("tbody tr").first.locator("td").first.click()
-    page.locator(".icon-crud-family").first.click()
+    page.locator("tbody tr").nth(0).locator("td").nth(0).click()
+    page.locator(".icon-crud-family").nth(0).click()
 
-    with page.expect_download() as download_info:
-        page.get_by_text("Download Client's Format").click()
+    if company_format == "sigmatech":
+        with page.expect_download() as download_info:
+            page.get_by_text("Download Sigmatech's Format").click()
+    else:
+        with page.expect_download() as download_info:
+            page.get_by_text("Download Client's Format").click()
 
     download = download_info.value
 
-    path = "Downloaded_timesheet/" + download.suggested_filename
+    path = f"{BASE_DIR}/employee/Downloaded_timesheet/{company_format.upper()}/" + f"{env_config.upper()} - Employee - {company_format} - {timesheet_config} - {download.suggested_filename}"
     download.save_as(path)
-
-    print("Downloaded file:", download.suggested_filename)
-    print("Saved to:", path)
     
     context.close()
     browser.close()
--- February-26/employee/employee_timesheet_merged.py
+++ February-26/employee/employee_timesheet_merged.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 from hr.set_format import set_employee_format
 from employee.employee_timesheet_download import download_timesheet
+from hr.hr_timesheet_merged import timesheet_employee
 from playwright.sync_api import Playwright
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,7 +43,7 @@ def create_timesheet(page, timesheet_type, fmt):
         "full": ("Sunday, February 1st,", "Saturday, February 28th,"),
         "halfh": ("Sunday, February 1st,", "Saturday, February 14th,"),
         "halft": ("Sunday, February 15th,", "Saturday, February 28th,"),
-        "last": ("Sunday, February 25th,", "Saturday, February 28th,"),
+        "last": ("Wednesday, February 25th,", "Saturday, February 28th,"),
     }
 
     start_date, end_date = timesheet_map[timesheet_type]
@@ -55,17 +56,20 @@ def create_timesheet(page, timesheet_type, fmt):
     page.locator("input[name='end_date']").click()
     page.get_by_role("button", name=f"Choose {end_date}").click()
 
+    # Comment
+    comment = "timesheet has been Created by Automation -D."
+
     # SUBMIT
     if fmt == "bri" or fmt == "cimb":
-        page.locator("input[name=\"custom_fields[0].value\"]").fill(f"{fmt.upper()} timesheet has been Created by Automation -D.")
+        page.locator("input[name=\"custom_fields[0].value\"]").fill(f"{fmt.upper()} {comment}")
     elif fmt == "mandiri":
-        page.locator("input[name=\"custom_fields[0].value\"]").fill(f"{fmt.upper(), 1} timesheet has been Created by Automation -D.")
-        page.locator("input[name=\"custom_fields[1].value\"]").fill(f"{fmt.upper(), 2} timesheet has been Created by Automation -D.")
+        page.locator("input[name=\"custom_fields[0].value\"]").fill(f"{fmt.upper(), 1} {comment}")
+        page.locator("input[name=\"custom_fields[1].value\"]").fill(f"{fmt.upper(), 2} {comment}")
     elif fmt == "hypernet":
-        page.locator("input[name=\"custom_fields[0].value\"]").fill(f"{fmt.upper(), 1} timesheet has been Created by Automation -D.")
-        page.locator("input[name=\"custom_fields[1].value\"]").fill(f"{fmt.upper(), 2} timesheet has been Created by Automation -D.")
-        page.locator("input[name=\"custom_fields[2].value\"]").fill(f"{fmt.upper(), 3} timesheet has been Created by Automation -D.")
-        page.locator("input[name=\"custom_fields[3].value\"]").fill(f"{fmt.upper(), 4} timesheet has been Created by Automation -D.")
+        page.locator("input[name=\"custom_fields[0].value\"]").fill(f"{fmt.upper(), 1} {comment}")
+        page.locator("input[name=\"custom_fields[1].value\"]").fill(f"{fmt.upper(), 2} {comment}")
+        page.locator("input[name=\"custom_fields[2].value\"]").fill(f"{fmt.upper(), 3} {comment}")
+        page.locator("input[name=\"custom_fields[3].value\"]").fill(f"{fmt.upper(), 4} {comment}")
 
     page.get_by_role("button", name="Create").click()
     page.get_by_role("link", name="Close").click()
@@ -73,15 +77,68 @@ def create_timesheet(page, timesheet_type, fmt):
 
 # ================= TEST =================
 
-def test_create_timesheet(playwright: Playwright, page, timesheet_config, env_config, company_format):
-    # Set employee timesheet format
-    set_employee_format(playwright, env_config, company_format)
+# def test_create_timesheet(playwright: Playwright, page, timesheet_config, env_config, company_format):
+#     # Set employee timesheet format from HR module
+#     set_employee_format(playwright, env_config, company_format)
 
-    # Create timesheet
+#     # Create timesheet employee
+#     login_page(page, env_config)
+#     create_timesheet(page, timesheet_config, company_format)
+
+#     # Download timesheet employee
+#     download_timesheet(playwright, env_config, company_format, timesheet_config)
+
+#     # Approve-Reject timesheet from HR module
+#     timesheet_employee(playwright, env_config, company_format, timesheet_config)
+
+
+# ================= AUTO =================
+
+def create_employee_timesheet(playwright: Playwright, env_config, company_format, timesheet_config):
+
+    browser = playwright.chromium.launch(headless=False)
+
+    # context baru → reset session
+    context = browser.new_context()
+    page = context.new_page()
+
+    # login
     login_page(page, env_config)
+
+    # create timesheet
     create_timesheet(page, timesheet_config, company_format)
 
-    # Download timesheet
-    download_timesheet(playwright, env_config)
+    # download
+    download_timesheet(playwright, env_config, company_format, timesheet_config)
 
-    
+    # HR action
+    timesheet_employee(playwright, env_config, company_format, timesheet_config)
+
+    context.close()
+    browser.close()
+
+
+TIMESHEET_TYPES = ["full", "halfh", "halft", "last"]
+
+FORMATS = [
+    "sigmatech",
+    "bri",
+    "cimb",
+    "mandiri"
+]
+
+
+def test_timesheet_merged(playwright, env_config):
+
+    for company_format in FORMATS:
+
+        set_employee_format(playwright, env_config, company_format)
+
+        for timesheet_type in TIMESHEET_TYPES:
+
+            create_employee_timesheet(
+                playwright,
+                env_config,
+                company_format,
+                timesheet_type
+            )
